9b1a94f367adc273a1c8190ca8be6b96dac631ad
Jarka <jarmila.krupova@vsb.cz>
2021-10-04 11:50:33 +0200

    Initial commit

---
+++ WeatherApp/CitiesActivity.cs
@@ -0,0 +1,70 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeatherApp {
+	[Activity(Label = "CitiesActivity")]
+	public class CitiesActivity : Activity {
+		TextView textViewOstrava;
+		TextView textViewBruntal;
+		TextView textViewHelsinki;
+		TextView textViewRome;
+		TextView textViewOttawa;
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.cities_layout);
+			SetupReferences();
+			SubscribeEventHandlers();
+		}
+
+		private void SetupReferences() {
+			textViewBruntal = FindViewById<TextView>(Resource.Id.textViewBruntal);
+			textViewHelsinki = FindViewById<TextView>(Resource.Id.textViewHelsinki);
+			textViewOstrava = FindViewById<TextView>(Resource.Id.textViewOstrava);
+			textViewOttawa = FindViewById<TextView>(Resource.Id.textViewOttawa);
+			textViewRome = FindViewById<TextView>(Resource.Id.textViewRome);
+		}
+
+		private void SubscribeEventHandlers() {
+			textViewBruntal.Click += TextViewBruntal_Click;
+			textViewHelsinki.Click += TextViewHelsinki_Click;
+			textViewOstrava.Click += TextViewOstrava_Click;
+			textViewOttawa.Click += TextViewOttawa_Click;
+			textViewRome.Click += TextViewRome_Click;
+		}
+
+		private void TextViewRome_Click(object sender, EventArgs e) {
+			GoBackWithCity("Rome");
+		}
+
+		private void GoBackWithCity(string city) {
+			Intent intent = new Intent();
+			intent.PutExtra("City", city);
+			SetResult(Result.Ok, intent);
+			Finish();
+		}
+
+		private void TextViewOttawa_Click(object sender, EventArgs e) {
+		GoBackWithCity("Ottawa");
+		}
+
+		private void TextViewOstrava_Click(object sender, EventArgs e) {
+			GoBackWithCity("Ostrava");
+		}
+
+		private void TextViewHelsinki_Click(object sender, EventArgs e) {
+			GoBackWithCity("Helsinki");
+		}
+
+		private void TextViewBruntal_Click(object sender, EventArgs e) {
+			GoBackWithCity("Bruntal");
+		}
+	}
+}
---
+++ WeatherApp/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace WeatherApp {
+	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity {
+		ImageView imageViewPicture;
+		TextView textViewCity;
+		TextView textViewWeather;
+		TextView textViewTemperature;
+		TextView textViewWind;
+		TextView textViewHumidity;
+		TextView textViewLocalTime;
+		Button buttonChange;
+
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.activity_main);
+			SetupReferences();
+			SubscribeEventHandlers();
+		}
+
+		private void SubscribeEventHandlers() {
+			buttonChange.Click += ButtonChange_Click;
+		}
+
+		private void ButtonChange_Click(object sender, EventArgs e) {
+			Intent intent = new Intent(this, typeof(CitiesActivity));
+			StartActivityForResult(intent, 1);
+		}
+
+		protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data) {
+			if (requestCode == 1 && resultCode == Result.Ok) {
+				string city = data.GetStringExtra("City");
+				textViewCity.Text = city;
+			}
+		}
+
+		private void SetupReferences() {
+			imageViewPicture = FindViewById<ImageView>(Resource.Id.imageViewPicture);
+			textViewCity = FindViewById<TextView>(Resource.Id.textViewCity);
+			textViewWeather = FindViewById<TextView>(Resource.Id.textViewWeather);
+			textViewTemperature = FindViewById<TextView>(Resource.Id.textViewTemperature);
+			textViewWind = FindViewById<TextView>(Resource.Id.textViewWind);
+			textViewHumidity = FindViewById<TextView>(Resource.Id.textViewHumidity);
+			textViewLocalTime = FindViewById<TextView>(Resource.Id.textViewLocalTime);
+			buttonChange = FindViewById<Button>(Resource.Id.buttonChange);
+		}
+	}
+}
